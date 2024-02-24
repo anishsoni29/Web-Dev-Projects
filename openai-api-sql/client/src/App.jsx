37dc686e186752c6bd69bdac7fca712fdf25c0ee
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-24 23:22:16 +0530

    added the frontend code

--- openai-api-sql/client/src/App.jsx
+++ openai-api-sql/client/src/App.jsx
@@ -1,26 +1,39 @@
 import styles from "./index.module.css";
 import sqlLogo from "./assets/sqlLogo.png";
 
-function App() {
-  return (
-    <body className={styles.body}>
-      <img src={sqlLogo} alt="" className={styles.icon} />
-      <h3 className={styles.h3}>Generate SQL with AI</h3>
+import { useState } from "react";
 
-      <form className={styles.form}>
-        <input
-          className={styles.inputText}
-          type="text"
-          name="query-description"
-          placeholder="Describe your query"
-        />
-        <input
-          className={styles.inputSubmit}
-          type="submit"
-          value="Generate query"
-        />
-      </form>
-    </body>
+function App() {
+  const [queryDescription, setQueryDescription] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted", queryDescription); //returning the query description
+  };
+
+  return (
+    <>
+      <body className={styles.body}>
+        <img src={sqlLogo} alt="" className={styles.icon} />
+        <h3 className={styles.h3}>Generate SQL with AI</h3>
+
+        <form onSubmit={onSubmit} className={styles.form}>
+          <input
+            className={styles.inputText}
+            type="text"
+            name="query-description"
+            placeholder="Describe your query"
+            onChange={(e) => {
+              setQueryDescription(e.target.value);
+            }}
+          />
+          <input
+            className={styles.inputSubmit}
+            type="submit"
+            value="Generate query"
+          />
+        </form>
+      </body>
+    </>
   );
 }
 
